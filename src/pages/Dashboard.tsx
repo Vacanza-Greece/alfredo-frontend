9639d68b0e9866c642c76b3e0965b82a5ff8dfee
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-09-15 01:33:01 +0600

    implemented second code in plane widget in trip.com

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -507,13 +507,13 @@ const Dashboard = () => {
               ) : (
                 <iframe
                   key="mobile"
-                  src="https://www.trip.com/partners/ad/S19755339?Allianceid=9679412&SID=326856440&trip_sub1="
+                  src="https://www.trip.com/partners/ad/S19755346?Allianceid=9679412&SID=326856440&trip_sub1="
                   width="320"
-                  height="640"
+                  height="480"
                   frameBorder="0"
                   scrolling="auto"
                   style={{ border: "none", display: "block", maxWidth: "100%" }}
-                  id="S19755339"
+                  id="S19755346"
                   title="Trip.com Flight Booking"
                 />
               )}
